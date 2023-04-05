c2a71fd50c9c89ad16b227ac7774f75ed319ad84
chceol <mine3386@naver.com>
2023-04-05 23:01:39 +0900

    struct.c파일 추가

--- homework3/padding.c
+++ homework3/padding.c
@@ -10,9 +10,9 @@ int main() {
 
     struct student pst;
 
-    printf("size of student = %ld\n", sizeof(struct student));    /* 13 bytes */    //선언한 구조체 자료형의 크기를 출력한다. 
-    printf("size of int = %ld\n", sizeof(int));                  /* 4 bytes */
-    printf("size of short = %ld\n", sizeof(short));             /* 2 bytes */
+    printf("size of student = %ld\n", sizeof(struct student));    /* 13 bytes */    //선언한 구조체 자료형의 크기를 출력한다. 19바이트가 출력될 것으로 예상되지만 패딩에 의해 24바이트가 출력된다.
+    printf("size of int = %ld\n", sizeof(int));                  /* 4 bytes */      //4바이트가 출력된다.
+    printf("size of short = %ld\n", sizeof(short));             /* 2 bytes */       //2바이트가 출력된다.
 
     return 0;
 }
